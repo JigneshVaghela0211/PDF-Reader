8126d8bed318f2549fd38a6ab28766b09735226e
Jignesh Vaghela <jigneshv@hyperlinkinfosystem.net.in>
2024-08-05 18:43:06 +0530

    [+] Migrate groovy to Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.hilt)
+    id("kotlin-kapt")
+    id("kotlin-android")
+}
+
+android {
+    namespace = "com.pdf.pdfreader"
+    compileSdkVersion(34)
+
+    defaultConfig {
+        applicationId = "com.pdf.pdfreader"
+        minSdkVersion(24)
+        targetSdkVersion(34)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    //UI components
+    implementation(libs.androidx.constraintlayout)
+    implementation (libs . androidx . appcompat)
+            implementation (libs . material)
+
+            implementation (libs . androidx . core . ktx)
+            implementation (libs . androidx . activity)
+            // Hilt
+            implementation (libs . hilt . android)
+            kapt (libs . hilt . compiler)
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
+}
